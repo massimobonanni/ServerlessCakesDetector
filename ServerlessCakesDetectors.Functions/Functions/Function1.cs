d7e36e6255b237c45c5c83391563099c43ae49a5
Massimo Bonanni <massimo.bonanni@tiscali.it>
2022-11-24 08:29:19 +0100

    Add projects core and cognitive

--- ServerlessCakesDetectors.Functions/Functions/Function1.cs
+++ ServerlessCakesDetectors.Functions/Functions/Function1.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace ServerlessCakesDetectors.Functions
+namespace ServerlessCakesDetectors.Functions.Functions
 {
     public static class Function1
     {
